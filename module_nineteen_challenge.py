e7f7474bcb893a48343a6efa25d76476208bd8b6
JohnFelder <93882303+JohnFelder@users.noreply.github.com>
2022-05-15 16:26:54 -0400

    Add files via upload

--- module_nineteen_challenge.py
+++ module_nineteen_challenge.py
@@ -100,7 +100,7 @@ candidate_database = {
 people = ["Lane", "Ash", "Jo", "Kendall"]
 
 
-def get_people(w3):
+def get_people():
     """Display the database of Fintech Finders candidate information."""
     db_list = list(candidate_database.values())
 
@@ -283,7 +283,7 @@ if st.sidebar.button("Send Transaction"):
 
 # The function that starts the Streamlit application
 # Writes FinTech Finder candidates to the Streamlit page
-get_people()
+w3
 
 ################################################################################
 # Step 3: Inspect the Transaction
